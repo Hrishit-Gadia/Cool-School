91fcd7724da8d65448b775f2e0a662eb3bc524ad
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-06-20 10:39:05 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -9,14 +9,35 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-function addroom() {
+var newname = localStorage.getItem("Name");
+document.getElementById("Name").innerHTML = "Welcome " + newname + " !";
+
+function Add() {
     var Name = document.getElementById("Roomname").value;
     firebase.database().ref("/").child(Name).update({
-        RoomName : "Working?"
+        UserName: newname
     });
+
+    window.location = "Chat.html";
+    document.getElementById("Roomname").value = "";
+    localStorage.setItem("Room", Name);
 }
 
-function update() {
-    var newname = localStorage.getItem("Name");
-    document.getElementById("Name").innerHTML = newname;
+function getData() {
+    firebase.database().ref("/").on('value', function (snapshot) {
+          document.getElementById("output").innerHTML = "";
+          snapshot.forEach(function (childSnapshot) {
+                childKey = childSnapshot.key;
+                Room_names = childKey;
+                console.log(Room_names);
+                var row = "<div id = " + Room_names + " class = 'room_name' onclick = 'go(this.id)'> @" + Room_names + "</div> <Hr>";
+                document.getElementById("output").innerHTML += row ;
+          });
+    });
+}
+getData();
+
+function go(name) {
+    window.location = "Chat.html";
+    localStorage.setItem("RoomName", name);
 }
